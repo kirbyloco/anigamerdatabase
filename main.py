4c1a45f1c3fe8870d77d021b8a8066322c09d405
耀風 <adwin82627459@gmail.com>
2020-07-19 08:27:30 +0800

    test

---
+++ main.py
@@ -0,0 +1,31 @@
+import json
+import re
+
+import requests
+from lxml import etree
+
+snlist = []
+db = {}
+
+def load_snlist():
+    with open('sn_list.txt', 'r') as f:
+        a = f.readlines()
+    for _ in a:
+        snlist.append(re.findall(r'(\d+)', _)[0])
+
+def get_sn(sn):
+    a = requests.get(f'https://ani.gamer.com.tw/animeVideo.php?sn={sn}')
+    b = etree.HTML(a.text)
+    animename = re.findall(r'(.*) ', b.xpath('//div[@class="anime_name"]/h1')[0].text)[0]
+    print(animename)
+    db[animename] = {}
+    for _ in b.xpath('//section[@class="season"]/ul/li/a'):
+        db[animename][_.text] = re.findall(r'(\d+)', _.get('href'))[0]
+    if db[animename] == {}:
+        db[animename][re.findall(r'\[(.*)\]', b.xpath('//div[@class="anime_name"]/h1')[0].text)[0]] = sn
+
+load_snlist()
+for _sn in snlist:
+    get_sn(_sn)
+with open('anigamer.json', 'w', encoding='utf-8') as f:
+    json.dump(db, f, ensure_ascii=False)
---
+++ sn_update.py
@@ -0,0 +1,41 @@
+import requests
+import re
+from lxml import etree
+
+def checklastpage():
+    a = etree.HTML(html.text)
+    return a.xpath('//div[@class="page_number"]/a')[-1].text
+
+def getrealvideoid(url):
+    html = requests.get('https://ani.gamer.com.tw/' + url)
+    if html.text.find('18UP') == -1: #檢測是否為R-18
+        return re.findall("=(\d*)", html.url)[0]
+    else:
+        return '' + re.findall("=(\d*)", html.url)[0]
+
+def getdata(_):
+    b = ''
+    ANIGAMER_URL = 'https://ani.gamer.com.tw/animeList.php?page=' + str(_) + '&c=0&sort=0'
+    html = requests.get(ANIGAMER_URL)
+    a = etree.HTML(html.text)
+    animelist = a.xpath('//ul[@class="anime_list"]/li')
+    for _ in animelist:
+        with open('sn_list.txt', 'a', encoding='UTF-8') as f:
+            f.write('{0} all {1}\r'.format(getrealvideoid(_.xpath('a')[0].get('href')), _.xpath('div[@class="info"]/b')[0].text))
+
+def cleansntxt():
+    with open('sn_list.txt', 'w+', encoding='UTF-8') as f:
+        f.write('')
+
+def getlastpageid():
+    ANIGAMER_URL = 'https://ani.gamer.com.tw/animeList.php'
+    html = requests.get(ANIGAMER_URL)
+    a = etree.HTML(html.text)
+    return int(a.xpath('//div[@class="page_number"]/a')[-1].text) + 1
+
+if __name__ == '__main__':
+    print('清除舊的sn_list.txt')
+    cleansntxt()
+    for _ in range(1, getlastpageid()):
+        print('正在抓取第{0}頁'.format(_))
+        getdata(_)
