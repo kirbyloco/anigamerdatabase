7403502d630d25cd454027304f5acb5c41a95aa1
耀風 <adwin82627459@gmail.com>
2020-07-25 21:33:11 +0800

    二度修正多標籤和未抓到R18

--- main.py
+++ main.py
@@ -16,18 +16,19 @@ def load_snlist():
 def get_sn(sn):
     a = requests.get(f'https://ani.gamer.com.tw/animeVideo.php?sn={sn}')
     b = etree.HTML(a.text)
-    animename = (re.findall(r'(.*) \[.*\] \[.*\]', b.xpath('//div[@class="anime_name"]/h1')[0].text) or re.findall(r'(.*) ', b.xpath('//div[@class="anime_name"]/h1')[0].text))[0]
-    print(animename)
+    print(b.xpath('//div[@class="anime_name"]/h1')[0].text)
+    animename = re.findall(r'(.*) \[(電影|特別篇|\d.*)\]', b.xpath('//div[@class="anime_name"]/h1')[0].text)[0][0]
     db[animename] = {}
     index = []
-    # len(b.xpath('//section[@class="season"]/p'))
+    if len(b.xpath('//section[@class="season"]/p')) > 1:
+        sectag = b.xpath('//section[@class="season"]/p')[1].text
     for _ in b.xpath('//section[@class="season"]/ul/li/a'):
         if _.text not in index:
             db[animename][_.text] = re.findall(r'(\d+)', _.get('href'))[0]
             index.append(_.text)
         else:
-            db[animename][f'特別篇 {_.text}'] = re.findall(r'(\d+)', _.get('href'))[0]
-        
+            db[animename][f'{sectag} {_.text}'] = re.findall(r'(\d+)', _.get('href'))[0]
+
     if db[animename] == {}:
         db[animename][re.findall(r'\[(.*)\]', b.xpath('//div[@class="anime_name"]/h1')[0].text)[0]] = sn
 
