3b82507acff85a8c9ee2bf7a060ef8e48308e808
耀風 <adwin82627459@gmail.com>
2020-07-25 19:29:33 +0800

    修正本篇和特別篇

--- main.py
+++ main.py
@@ -16,11 +16,18 @@ def load_snlist():
 def get_sn(sn):
     a = requests.get(f'https://ani.gamer.com.tw/animeVideo.php?sn={sn}')
     b = etree.HTML(a.text)
-    animename = re.findall(r'(.*) ', b.xpath('//div[@class="anime_name"]/h1')[0].text)[0]
+    animename = (re.findall(r'(.*) \[.*\] \[.*\]', b.xpath('//div[@class="anime_name"]/h1')[0].text) or re.findall(r'(.*) ', b.xpath('//div[@class="anime_name"]/h1')[0].text))[0]
     print(animename)
     db[animename] = {}
+    index = []
+    # len(b.xpath('//section[@class="season"]/p'))
     for _ in b.xpath('//section[@class="season"]/ul/li/a'):
-        db[animename][_.text] = re.findall(r'(\d+)', _.get('href'))[0]
+        if _.text not in index:
+            db[animename][_.text] = re.findall(r'(\d+)', _.get('href'))[0]
+            index.append(_.text)
+        else:
+            db[animename][f'特別篇 {_.text}'] = re.findall(r'(\d+)', _.get('href'))[0]
+        
     if db[animename] == {}:
         db[animename][re.findall(r'\[(.*)\]', b.xpath('//div[@class="anime_name"]/h1')[0].text)[0]] = sn
 
